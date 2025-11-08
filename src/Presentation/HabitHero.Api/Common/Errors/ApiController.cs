303d67f88d26d9ff0819f894af44915babb56025
PavelGamezo <pashasblv@gmail.com>
2025-11-08 20:02:15 +0300

    fix: restructured api problem handling

--- src/Presentation/HabitHero.Api/Common/Errors/ApiController.cs
+++ src/Presentation/HabitHero.Api/Common/Errors/ApiController.cs
@@ -1,5 +1,6 @@
 ﻿using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace HabitHero.Api.Common.Errors
 {
@@ -8,18 +9,46 @@ namespace HabitHero.Api.Common.Errors
     {
         protected IActionResult Problem(List<Error> errors)
         {
-            var firstError = errors.First();
+            if (errors.Count == 0)
+            {
+                return Problem();
+            }
 
-            var statusCode = firstError.Type switch
+            if (errors.All(error => error.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+
+            var error = errors.First();
+
+            return Problem(error);
+        }
+
+        protected IActionResult ValidationProblem(List<Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(
+                    error.Code,
+                    error.Description);
+            }
+
+            return ValidationProblem(modelStateDictionary);
+        }
+
+        private IActionResult Problem(Error error)
+        {
+            var statusCode = error.Type switch
             {
                 ErrorType.NotFound => StatusCodes.Status404NotFound,
                 ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
                 ErrorType.Conflict => StatusCodes.Status409Conflict,
-                ErrorType.Validation => StatusCodes.Status400BadRequest,
                 _ => StatusCodes.Status500InternalServerError
             };
 
-            return Problem(statusCode: statusCode, title: firstError.Description);
+            return Problem(statusCode: statusCode, title: error.Description);
         }
     }
 }
